bdf4a502fd87541aae144350508f3da8ff1b9fb1
Tanner <troxs690@gmail.com>
2022-07-12 12:18:41 -0500

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listing, setListings] = useState([]);
+  const[currentSearch, setSearch] = useState('')
+  
+
+  
+  function handleSearch(search){
+    setSearch(search)   
+ 
+ }
+  
+ 
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(r => r.json())
+      .then(data => {
+        setListings(data);
+      });
+  },[]);
+
   return (
-    <div className="app">
-      <Header />
-      <ListingsContainer />
+    <div  className="app">
+      <Header onSearch={handleSearch}/>
+      <ListingsContainer currentSearch={currentSearch} listing={listing} setListings={setListings}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,38 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listing, setListings, currentSearch }) {
+  function handleListingDelete(id) {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+      .then(r => r.json())
+      .then(() => {
+        const deleteListing = listing.filter(l => l.id !== id);
+        setListings(deleteListing);
+      });
+  }
+
+  const handleSearch = listing.filter(list => {
+    return list.description.toLowerCase().includes(currentSearch.toLowerCase())
+  });
+
+
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {handleSearch.map(listingg => (
+          <ListingCard
+            listing={listing}
+            deleteListing={handleListingDelete}
+            key={listingg.id}
+            id={listingg.id}
+            image={listingg.image}
+            description={listingg.description}
+            location={listingg.location}
+          />
+        ))}
       </ul>
     </main>
   );
